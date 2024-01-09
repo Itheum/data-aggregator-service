65735f9e79cf0a3dd654aa83a81bca3e8d358596
Micha Vie <micha@freeiam.com>
2024-01-09 23:21:22 +0100

    update api sync endpoints

--- apps/api/src/endpoints/apps/apps.controller.ts
+++ apps/api/src/endpoints/apps/apps.controller.ts
@@ -35,14 +35,14 @@ export class AppsController {
   }
 
   // TODO: @UseGuards(NativeAuthGuard)
-  @Get('/apps/sync')
+  @Get('/sync-apps')
   @ApiResponse({ status: 200 })
   async syncAll() {
     await this.appService.syncApps()
   }
 
   // TODO: @UseGuards(NativeAuthGuard)
-  @Get('/apps/:appid/sync')
+  @Get('/apps/:appid/sync-delegations')
   @ApiResponse({ status: 200 })
   @ApiResponse({ status: 404 })
   async sync(@Param('appid') appId: string) {
